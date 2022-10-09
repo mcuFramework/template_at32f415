5611f9dde220a83f1ef213243b5f83f8bd7d7dec
ZxyKira <danny41553@gmail.com>
2022-10-09 23:33:22 +0800

    [fix] something

--- src/main/Lowlevel.cpp
+++ src/main/Lowlevel.cpp
@@ -8,58 +8,170 @@
 /* ****************************************************************************************
  * Include
  */  
-#include <stdio.h>
 
 //-----------------------------------------------------------------------------------------
 #include "mcuf.h"
 #include "driver.h"
 #include "core_arterytek_at32f415.h"
+#include "support_cmsisrtos.h"
+
 //-----------------------------------------------------------------------------------------
 
 /* ****************************************************************************************
  * Using
- */
+ */  
  
 //-----------------------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------------------
-using mcuf::lang::Thread;
+using arterytek::at32f415::Core;
+using arterytek::at32f415::CoreIomux;
+using arterytek::at32f415::serial::CoreSerialPort;
+using arterytek::at32f415::serial::CoreSerialPortReg;
+
+using mcuf::lang::System;
+using mcuf::lang::ErrorCode;
 
 /* ****************************************************************************************
  * Extern
  */
-
-/* ****************************************************************************************
- * Class
- */
+extern "C" void core_at32f415_interrupt_priority(void);
 
 /* ****************************************************************************************
  * Variable
  */
-hal::general::GeneralPin* led[8];
+static hal::serial::SerialPort* systemConsoleSerialPort;
+static mcuf::io::Console* systemConsole;
+
 
 /* ****************************************************************************************
  * Method
  */
 
-void setup(Thread* _this){
-  for(int i=0; i<8; ++i){
-    led[i] = new arterytek::at32f415::general::CoreGeneralPin(arterytek::at32f415::Core::gpiob, i);
-    led[i]->setOutput();
-    led[i]->setLow();
+/**
+ *
+ */
+bool errorCodeHandler(const void* address, ErrorCode code){
+  System::out().print("System Error:");
+  
+  switch(code){
+    case ErrorCode::NONE:
+      System::out().format("NONE\n");
+      break;
+    
+    case ErrorCode::HARD_FAULT:
+      System::out().format("HARD_FAULT\n");
+      break;
+    
+    case ErrorCode::SYSTEM_ERROR:
+      System::out().format("SYSTEM_ERROR\n");
+      break;
+    
+    case ErrorCode::OUT_OF_MEMORY:
+      System::out().format("OUT_OF_MEMORY\n");
+      break;
+    
+    case ErrorCode::OUT_OF_THREAD_STACK_MEMORY:
+      System::out().format("OUT_OF_THREAD_STACK_MEMORY\n");
+      break;
+    
+    case ErrorCode::NULL_POINTER:
+      System::out().format("NULL_POINTER\n");
+      break;
+    
+    case ErrorCode::ILLEGAL_ARGUMENT:
+      System::out().format("ILLEGAL_ARGUMENT\n");
+      break;
+    
+    case ErrorCode::INSUFFICIENT_MEMORY:
+      System::out().format("INSUFFICIENT_MEMORY\n");
+      break;
+    
+    case ErrorCode::WRITE_TO_READONLY_MEMORY:
+      System::out().format("WRITE_TO_READONLY_MEMORY\n");
+      break;
+    
+    case ErrorCode::MEMORY_NOT_ALIGNMENT_32BIT:
+      System::out().format("MEMORY_NOT_ALIGNMENT_32BIT\n");
+      break;
+    
+    case ErrorCode::MEMORY_NOT_ALIGNMENT_64BIT:
+      System::out().format("MEMORY_NOT_ALIGNMENT_64BIT\n");
+      break;
+    
+    case ErrorCode::RTX_STACK_UNDERFLOW:
+      System::out().format("RTX_STACK_UNDERFLOW\n");
+      break;
+    
+    case ErrorCode::RTX_ISR_QUEUE_OVERFLOW:
+      System::out().format("RTX_ISR_QUEUE_OVERFLOW\n");
+      break;
+    
+    case ErrorCode::RTX_TIMER_QUEUE_OVERFLOW:
+      System::out().format("RTX_TIMER_QUEUE_OVERFLOW\n");
+      break;
+    
+    case ErrorCode::RTX_CLIB_SPACE:
+      System::out().format("RTX_CLIB_SPACE\n");
+      break;
+    
+    case ErrorCode::RTX_CLIB_MUTEX:
+      System::out().format("RTX_CLIB_MUTEX\n");
+      break;
+  
+    case ErrorCode::RESERVED:
+      System::out().format("RESERVED\n");
+      break;
   }
-  System::out().print("init\n");
-  _this->delay(100);
+  
+  return true;
 }
 
-void loop(Thread* _this){
-  for(int i=0; i<8; ++i){
-    led[i]->setToggle();
-    _this->delay(250);
-  }
-  System::out().print("hello\n");
+/**
+ *
+ */
+void lowlevel_basicInit(void){
+  arterytek::at32f415::Core::setSystemCoreClock(144);
   
+  System::initialize();
+  System::getRegister().setInterfaceKernel(new cmsisrtos::CmsisRtosKernel());
+  System::getRegister().setInterfaceThread(new cmsisrtos::CmsisRtosThread());
+  System::getRegister().setInterfaceTimer(new cmsisrtos::CmsisRtosTimer());
+  System::setup();
+  
+  Core::iomux.init();
+  Core::gpioa.init();
+  Core::gpiob.init();
+  Core::gpioc.init();
+  Core::gpiod.init();
+  Core::gpiof.init();
+  Core::iomux.remapSWDIO(CoreIomux::MapSWDIO::JTAGDISABLE);
 }
+
+/**
+ *
+ */
+void lowlevel_console(void){
+  Core::gpioa.setFunction(2, false);
+  systemConsoleSerialPort = new CoreSerialPort(CoreSerialPortReg::REG_USART2, 1024);
+  systemConsoleSerialPort->init();
+  systemConsoleSerialPort->baudrate(128000);
+  
+  systemConsole = new mcuf::io::ConsoleSerialPort(*systemConsoleSerialPort, 128, 1024);
+  System::getRegister().setPrintStream(&systemConsole->out());
+  System::getRegister().setInputStreamBuffer(&systemConsole->in());
+  System::getRegister().setErrorCodeHandler(errorCodeHandler);
+}
+
+/**
+ *
+ */
+void lowlevel(void){
+  lowlevel_basicInit();
+  lowlevel_console();
+  core_at32f415_interrupt_priority();
+}  
+
  
 /* ****************************************************************************************
  * End of file
